efd7cc649e317be33e9864e17b9f13772acab022
Ambler <ambler.official@gmail.com>
2021-09-06 00:39:49 +0900

    트위터 수정

--- src/code.js
+++ src/code.js
@@ -1,27 +1,27 @@
 function getId(url) {
-    try {
-        const id = new URL(url).pathname.slice(-32);
-        if (id.match(/[0-9a-f]{32}/)) return id;
-        return '';
-    } catch (e) {
-        return '';
-    }
+  try {
+    const id = new URL(url).pathname.slice(-32);
+    if (id.match(/[0-9a-f]{32}/)) return id;
+    return "";
+  } catch (e) {
+    return "";
+  }
 }
 
 export default function code(data) {
-    const {
-        myDomain,
-        notionUrl,
-        slugs,
-        pageTitle,
-        pageDescription,
-        googleFont,
-        customScript,
-    } = data;
-    let url = myDomain.replace('https://', '').replace('http://', '');
-    if (url.slice(-1) === '/') url = url.slice(0, url.length - 1);
+  const {
+    myDomain,
+    notionUrl,
+    slugs,
+    pageTitle,
+    pageDescription,
+    googleFont,
+    customScript
+  } = data;
+  let url = myDomain.replace("https://", "").replace("http://", "");
+  if (url.slice(-1) === "/") url = url.slice(0, url.length - 1);
 
-    return `  /* CONFIGURATION STARTS HERE */
+  return `  /* CONFIGURATION STARTS HERE */
   
   /* Step 1: enter your domain name like fruitionsite.com */
   const MY_DOMAIN = '${url}';
@@ -34,22 +34,22 @@ export default function code(data) {
   const SLUG_TO_PAGE = {
     '': '${getId(notionUrl)}',
 ${slugs
-    .map(([pageUrl, notionUrl]) => {
-        const id = getId(notionUrl);
-        if (!id || !pageUrl) return '';
-        return `    '${pageUrl}': '${id}',\n`;
-    })
-    .join('')}  };
+  .map(([pageUrl, notionUrl]) => {
+    const id = getId(notionUrl);
+    if (!id || !pageUrl) return "";
+    return `    '${pageUrl}': '${id}',\n`;
+  })
+  .join("")}  };
   
   /* Step 3: enter your page title and description for SEO purposes */
-  const PAGE_TITLE = '${pageTitle || ''}';
-  const PAGE_DESCRIPTION = '${pageDescription || ''}';
+  const PAGE_TITLE = '${pageTitle || ""}';
+  const PAGE_DESCRIPTION = '${pageDescription || ""}';
   
   /* Step 4: enter a Google Font name, you can choose from https://fonts.google.com */
-  const GOOGLE_FONT = '${googleFont || ''}';
+  const GOOGLE_FONT = '${googleFont || ""}';
   
   /* Step 5: enter any custom scripts you'd like */
-  const CUSTOM_SCRIPT = \`${customScript || ''}\`;
+  const CUSTOM_SCRIPT = \`${customScript || ""}\`;
   
   /* CONFIGURATION ENDS HERE */
   
@@ -117,7 +117,7 @@ ${slugs
       return response;
     }
     let response;
-    if (url.pathname.startsWith('/app') && url.pathname.endsWith('js')) {
+    if (url.pathname.endsWith("js")) {
       response = await fetch(url.toString());
       let body = await response.text();
       response = new Response(body.replace(/www.notion.so/g, MY_DOMAIN).replace(/notion.so/g, MY_DOMAIN), response);
